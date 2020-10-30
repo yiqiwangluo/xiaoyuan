af3e71bed4c35ab0ebba58e8c8a5c0bb410ba2bc
stormzi <3304918725@qq.com>
2020-10-30 10:02:04 +0800

    路由跳转修改以及冲突处理

--- yiqi/src/components/header.vue
+++ yiqi/src/components/header.vue
@@ -41,9 +41,9 @@ export default {
         return {
             component_list: [
                 { title: '首页', url: '/' },
-                { title: '产品介绍', url: '/' },
+                { title: '产品介绍', url: '/product' },
                 { title: '公司简介', url: '/intro' },
-                { title: '联系我们', url: '/' }
+                { title: '联系我们', url: '/contact' }
             ],
             // 功能列表集合是否展开
             flag: false
--- yiqi/src/router/index.js
+++ yiqi/src/router/index.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/views/index'
 import Product from '@/views/product'
-<<<<<<< Updated upstream
 import Intro from '@/views/intro'
-=======
 import Contact from '@/views/contact'
->>>>>>> Stashed changes
 
 Vue.use(Router)
 
